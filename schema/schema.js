660e434ae4a0b0160018558b81238298c117c43b
rossoj85 <jason_ro@photoninfotech.net>
2018-12-24 13:16:33 -0600

    full CRUD backend

--- schema/schema.js
+++ schema/schema.js
@@ -8,7 +8,8 @@
      GraphQLString,
      GraphQLInt,
      GraphQLSchema,
-     GraphQLList
+     GraphQLList, 
+     GraphQLNonNull
  } = graphql;
 
  //iunstructs graphQL about what a user object type loks like
@@ -21,7 +22,7 @@
         users: {
             type: new GraphQLList(UserType),
             resolve(parentValue, args){
-                console.log(parentValue)
+               
                 return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
                 .then(res=>res.data)
             }
@@ -75,6 +76,50 @@
     
  });
 
+ const mutation = new GraphQLObjectType({
+      name: 'Mutation',
+      fields: {
+        addUser: {
+            type: UserType, //refers to the type of data were eventually going to return fromthe resolve function 
+            args: {
+                firstName: {type: new GraphQLNonNull(GraphQLString)},
+                age: {type: new GraphQLNonNull (GraphQLInt) }, 
+                companyId: {type: GraphQLString}
+            
+            },
+            resolve(parentValue, {firstName, age, companyId}){
+                console.log('HEllo')
+                return axios.post(`http://localhost:3000/users`,{firstName,age,companyId})
+                .then(res=>res.data)
+            }
+        },
+        deleteUser: {
+            type:UserType,
+            args: {
+                id: {type: new GraphQLNonNull(GraphQLString)}
+            },
+            resolve(parentValue,args){
+                console.log('Hello args', args)
+                return axios.delete(`http://localhost:3000/users/${args.id}`)
+                .then(res=>res.data)
+            }
+        },
+        editUser:{
+            type: UserType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLString)},
+                firstName: { type: GraphQLString},
+                age: { type: GraphQLInt},
+                companyId: { type: GraphQLString }
+            },
+            resolve(parentValue, args){
+                return axios.patch(`http://localhost:3000/users/${args.id}`,args)
+                .then(res=>res.data)
+            }
+        }
+      }
+ })
  module.exports = new GraphQLSchema({
-     query: RootQuery
+     query: RootQuery,
+     mutation
  }) 
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const expressGraphQL = require('express-graphql')
 const schema = require('./schema/schema');
+const volleyball = require('volleyball')
 
 const app = express()
-
+app.use(volleyball)
 app.use('/graphql', expressGraphQL({
     schema,
-    graphiql: true
+    graphiql: true 
 }))
 app.listen(4000, ()=>{
     console.log('Listening')
